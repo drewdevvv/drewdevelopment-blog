0562f0c4f6b78c1ee5d54173ab11188c929e7b22
Andrew Ponce <me@drewponce.dev>
2024-03-30 17:00:01 -0700

    update page.tsx
    s

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-2 flex flex-col lg:flex-row items-center">
+        <div className="py-8 flex flex-col lg:flex-row items-center">
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://github.com/drewdevvv"
