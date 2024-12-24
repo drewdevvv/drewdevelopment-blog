ff781120d2a3ce6db7ce7a59702bef2a3c928dbf
Andrew Ponce <me@drewponce.dev>
2024-12-24 14:38:07 -0800

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from "@/lib/constants";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Andrew <small className="text-2xl md:text-4xl text-gray-600 mr-2">(aka drew)</small>Ponce
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
